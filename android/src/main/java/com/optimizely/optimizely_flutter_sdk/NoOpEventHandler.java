c02abdc0255af75f538bf766a45f29d0e57c8aee
Joachim Schmidt <joachim.schmidt@appunite.com>
2025-04-29 23:17:13 +0200

    fix missing imports

--- android/src/main/java/com/optimizely/optimizely_flutter_sdk/NoOpEventHandler.java
+++ android/src/main/java/com/optimizely/optimizely_flutter_sdk/NoOpEventHandler.java
@@ -1,3 +1,6 @@
+package com.optimizely.optimizely_flutter_sdk;
+
+import android.content.Context;
 import com.optimizely.ab.android.event_handler.DefaultEventHandler;
 import com.optimizely.ab.event.LogEvent;
 
